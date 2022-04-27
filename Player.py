914ab04c64ef06248e7c513ee05026470679cad8
EdwinTomy <etomygeorg@miners.utep.edu>
2022-04-27 08:46:37 -0600

    has cv2.imshow() bug

---
+++ Player.py
@@ -0,0 +1,92 @@
+import cv2
+from threading import Thread, Semaphore, Lock
+
+STOP = 123
+DELAY = 42
+FILE = 'clip.mp4'
+
+
+class ProducerConsumerQ:
+
+    def __init__(self):
+        self.queue = []
+        self.full = Semaphore(0)
+        self.empty = Semaphore(10)
+        self.lock = Lock()
+
+    def put(self, item):  # Producer
+        self.empty.acquire()
+        self.lock.acquire()
+        self.queue.append(item)
+        self.lock.release()
+        self.full.release()
+
+    # Question
+    def get(self):  # Consumer
+        self.full.acquire()
+        self.lock.acquire()
+        frame = self.queue.pop(0)
+        self.lock.release()
+        self.empty.release()
+        return frame
+
+
+def extract_frames(file_name, output_q):
+    count = 0
+    vid_cap = cv2.VideoCapture(file_name)
+    success, image = vid_cap.read()
+
+    while success:
+        output_q.put(image)
+        success, image = vid_cap.read()
+        print(f'Reading frame {count} {success}')
+        count += 1
+
+    output_q.put(STOP)
+    print('Frame extraction complete')
+
+
+def convert_grayscale(input_q, output_q):
+    count = 0  # frame count
+    input_frame = input_q.get()  # get input colored frame
+
+    while type(input_frame) != int:
+        print(f'Converting frame {count}')
+        grayscale_frame = cv2.cvtColor(input_frame, cv2.COLOR_BGR2GRAY)
+        output_q.put(grayscale_frame)  # add frame to queue
+        count += 1
+        input_frame = input_q.get()  # get next input colored frame
+
+    output_q.put(STOP)
+    print('Frame conversion complete')
+    return
+
+
+def display_frames(input_q):
+    count = 0
+    input_frame = input_q.get()
+
+    while type(input_frame) != int:
+        print(f'Displaying frame {count}')
+        cv2.imshow('Video', input_frame)
+        if cv2.waitKey(DELAY) and 0xFF == ord("q"):
+            break
+
+        count += 1
+        input_frame = input_q.get()
+
+    print('Finished displaying all frames')
+    cv2.destroyAllWindows()
+
+
+extract_to_grayscale_q = ProducerConsumerQ()
+grayscale_to_display_q = ProducerConsumerQ()
+
+extract = Thread(target=extract_frames, args=(FILE, extract_to_grayscale_q))
+convert = Thread(target=convert_grayscale, args=(extract_to_grayscale_q, grayscale_to_display_q))
+display = Thread(target=display_frames, args=(grayscale_to_display_q,))
+
+extract.start()
+convert.start()
+display.start()
+
